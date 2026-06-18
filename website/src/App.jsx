0cb24e252bbf6a17424bc4103d4667fc7e9f6f07
CodeSpark45 <somvanshipavan2006@gmail.com>
2026-06-18 00:13:53 -0700

    fix: resolve AppShell unmount crash and visibilitychange memory leak (Closes #2285)
    Signed-off-by: CodeSpark45 <somvanshipavan2006@gmail.com>

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -112,6 +112,7 @@ const MentorshipDashboard = lazy(() => import('./pages/mentorship/MentorshipDash
 const StatusPage = lazy(() => import('./pages/StatusPage'));
 const LiveStreamPage = lazy(() => import('./pages/streaming/LiveStreamPage'));
 const SponsorsPage = lazy(() => import('./pages/sponsors/SponsorsPage'));
+const ResourcesPage = lazy(() => import('./pages/resources/ResourcesPage'));
 
 const MNH = 88,
   DNH = 64;
@@ -464,8 +465,8 @@ function AppShell() {
 
     return () => {
       alive = false;
-      clearInterval(interval);
       off('content:updated', onContentUpdated);
+      document.removeEventListener('visibilitychange', onVisibilityChange);
     };
   }, []);
 
